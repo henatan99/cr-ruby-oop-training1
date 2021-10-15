001f59f045fe08463737548c9da8f93fa760d2d7
Henok Mekonnen <henatan99@yahoo.com>
2021-10-15 08:33:11 +0300

    Fix rubocop offence for method length

--- main.rb
+++ main.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 # rubocop:disable Metrics/CyclomaticComplexity
-# rubocop:disable Metrics/MethodLength: Method has too many lines
 
 require_relative './example_school_library_decorator/book'
 require_relative './example_school_library_decorator/person'
@@ -86,43 +85,49 @@ class App
   end
 end
 
+def options
+  puts 'Please choose an option by entering a number'
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a rental'
+  puts '6 - List all rentals for a given person id'
+  puts '7 - Exit'
+end
+
+def dispatch(response, app)
+  case response
+  when '1'
+    app.list_books
+  when '2'
+    app.list_people
+  when '3'
+    app.create_person
+  when '4'
+    app.create_book
+  when '5'
+    app.create_rental
+  when '6'
+    app.list_rentals_for_person_id
+  when '7'
+    puts 'Thank you for using this app!'
+  end
+end
+
 def main
   app = App.new
   puts "Welcome to school library app \n\n"
   response = nil
 
   while response != '7'
-    puts 'Please choose an option by entering a number'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
-
+    options
     response = gets.chomp
 
-    case response
-    when '1'
-      app.list_books
-    when '2'
-      app.list_people
-    when '3'
-      app.create_person
-    when '4'
-      app.create_book
-    when '5'
-      app.create_rental
-    when '6'
-      app.list_rentals_for_person_id
-    when '7'
-      puts 'Thank you for using this app!'
-    end
+    dispatch(response, app)
   end
 end
 
 main
 
 # rubocop:enable Metrics/CyclomaticComplexity
-# rubocop:enable Metrics/MethodLength: Method has too many lines
